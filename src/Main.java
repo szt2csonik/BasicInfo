978b623c4547f9c7de74ee15833b5dab93bc8be4
Your Name <szt2csonik@vasvari.org>
2023-09-14 13:10:04 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String name="Csorba Nikolasz";
+        System.out.println("name: " + name);
+        int age=21;
+        System.out.println("age: " + age);
+        double height=1.7;
+        System.out.println("height: " + height+"m");
+        boolean married=false;
+        System.out.println("married: " + married);
+    }
+}
